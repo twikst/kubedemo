f95d59bcaf94b646f588d610b33614393bbb60aa
twikst <61151555+twikst@users.noreply.github.com>
2022-09-30 10:22:18 +0300

    Create installContainerRuntime.sh

---
+++ installContainerRuntime.sh
@@ -0,0 +1,31 @@
+# Original: https://www.itzgeek.com/how-tos/linux/ubuntu-how-tos/install-containerd-on-ubuntu-22-04.html
+
+# Install Containerd
+wget https://github.com/containerd/containerd/releases/download/v1.6.8/containerd-1.6.8-linux-amd64.tar.gz
+sudo tar Czxvf /usr/local containerd-1.6.8-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mv containerd.service /usr/lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+sudo systemctl status containerd
+
+# Install runC
+wget https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+# Containerd configuration for Kubernetes
+sudo mkdir -p /etc/containerd/
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+# Install CNI Plugins For Containerd
+sudo mkdir -p /opt/cni/bin/
+sudo wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+# Restart the Containerd service
+sudo systemctl restart containerd
+
+# Install nerdctl (CLI)
+wget https://github.com/containerd/nerdctl/releases/download/v0.23.0/nerdctl-0.23.0-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local/bin nerdctl-0.19.0-linux-amd64.tar.gz
